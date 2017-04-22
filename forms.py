0b61f53f65471a5a00e06ee2a858704de00a2e4e
Kovriga <kovriga.dmitry.25@gmail.com>
2017-04-22 06:47:49 +0300

    form metaclass

--- forms.py
+++ forms.py
@@ -1,4 +1,15 @@
-from wtforms import Form
+from fields import BaseField
+
+
+class FormMeta(type):
+    def __new__(mcls, name, bases, namespace):
+        namespace['_fields'] = property(lambda self: {
+            key: value.copy()
+            for key, value in namespace.items()
+            if isinstance(value, BaseField)
+        })
+
+        super(FormMeta, mcls).__new__(name, bases, namespace)
 
 
 class BaseForm(object):
@@ -7,10 +18,10 @@ class BaseForm(object):
         self._data = data
         self._structure = {}
         self._validated = False
-        to_delete = [field for field in self._data if field not in self.fields]
+        to_delete = [field for field in self._data if field not in self._fields]
         for field in to_delete:
             del self._data[field]
-        for field_name, field in self.fields.items():
+        for field_name, field in self._fields.items():
             self._structure[field_name] = field(self, self.get(field_name), field_name)
 
     def validate(self):
@@ -31,4 +42,3 @@ class BaseForm(object):
 
     def get(self, name):
         return self._data.get(name)
-
